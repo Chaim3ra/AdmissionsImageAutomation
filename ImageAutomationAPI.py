5cf97db6f2929d066e658642c74cb287bafd3aed
Chaitanya Agarwal <chaitanya2205@gmail.com>
2019-02-06 12:52:07 +0530

    Added necessary documentation

--- ImageAutomationAPI.py
+++ ImageAutomationAPI.py
@@ -40,6 +40,7 @@ def requires_auth(f):
         return f(*args, **kwargs)
     return decorated
 
+#Function to convert RGB to HSV
 def rgb_to_hsv(r, g, b):
     maxc = max(r, g, b)
     minc = min(r, g, b)
@@ -62,6 +63,7 @@ def rgb_to_hsv(r, g, b):
 GREEN_RANGE_MIN_HSV = (80, 100, 100)
 GREEN_RANGE_MAX_HSV = (185, 255, 255)
 
+#Function to detect a face and autocrop
 def facecrop(image):
     
     facedata = '/anaconda3/lib/python3.6/site-packages/cv2/data/haarcascade_frontalface_alt.xml'
@@ -78,7 +80,7 @@ def facecrop(image):
         x, y, w, h = [ v for v in f ]
         #cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,255,0))
 
-        sub_face = img[y-math.floor(0.50*h):y+h+math.floor(0.40*h), x-math.floor(0.20*w):x+w+math.floor(0.20*w)]
+        sub_face = img[y-math.floor(0.50*h):y+h+math.floor(0.40*h), x-math.floor(0.20*w):x+w+math.floor(0.20*w)]#cropping the image
         fname, ext = os.path.splitext(image)
         #sub_face.save(os.path.join(app.config['CROPPED_IMAGES'], fname+ext))
         cv2.imwrite(fname+ext, sub_face)
@@ -93,8 +95,9 @@ def facecrop(image):
 @app.route('/')
 @requires_auth
 def index():
-   return render_template('ImagePreview.html')
+   return render_template('ImagePreview.html')#Load HTML page
 
+#Function to return the number of images in a folder
 @app.route('/count')
 def countImages():
     path1, dirs1, files_raw = next(os.walk(app.config['RAW_IMAGES']))
@@ -114,19 +117,19 @@ def compute():
         img1.save(os.path.join(app.config['RAW_IMAGES'], secure_filename(img1.filename)))
         facecrop(os.path.join(app.config['CROPPED_IMAGES'], filename))
         im=Image.open(os.path.join(app.config['CROPPED_IMAGES'], filename))
-        im=im.convert('RGB')
+        im=im.convert('RGB')#Convert image to RBG color space
         pix=im.load()
-        width,height=im.size
-        for x in range(width):
-            for y in range(height):
+        width,height=im.size#image dimensions
+        for x in range(width):#traverse each pixel horizontally
+            for y in range(height):#traverse each pixel vertically
                 r, g, b = pix[x, y]
-                h_ratio, s_ratio, v_ratio = rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)
+                h_ratio, s_ratio, v_ratio = rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)#function call to convert RGB values
                 h, s, v = (h_ratio * 360, s_ratio * 255, v_ratio * 255)
                 min_h, min_s, min_v = GREEN_RANGE_MIN_HSV
                 max_h, max_s, max_v = GREEN_RANGE_MAX_HSV
-                if min_h <= h <= max_h and min_s <= s <= max_s and min_v <= v <= max_v:
-                    pix[x, y] = (255, 255, 255,1)
-        im.save(os.path.join(app.config['PROCESSED_IMAGES'], filename))
+                if min_h <= h <= max_h and min_s <= s <= max_s and min_v <= v <= max_v:#check if pixel color is within the range
+                    pix[x, y] = (255, 255, 255,1)#set pixel value to white
+        im.save(os.path.join(app.config['PROCESSED_IMAGES'], filename))#save processed image
         before=os.path.join(app.config['CROPPED_IMAGES'], filename)
         after=os.path.join(app.config['PROCESSED_IMAGES'], filename)
     return render_template('ImagePreview.html', user_image = before, user_image2= after)
@@ -142,4 +145,4 @@ def compute():
 ##api.add_resource(upload,"/upload")
 
 if __name__=='__main__':
-	app.run(debug=True)
+	app.run(debug=True)#remove when deploying
